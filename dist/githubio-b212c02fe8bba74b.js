19b0ec40fcefcca66567ab83813405bc7c71fc98
Eric <ericmachmer@factualfiction.com>
2024-08-31 09:04:36 -0700

    "Lorem ipsum" layout in css

--- dist/githubio-b212c02fe8bba74b.js
+++ dist/githubio-b212c02fe8bba74b.js
@@ -1,26 +1,10 @@
 let wasm;
 
-const cachedTextDecoder = (typeof TextDecoder !== 'undefined' ? new TextDecoder('utf-8', { ignoreBOM: true, fatal: true }) : { decode: () => { throw Error('TextDecoder not available') } } );
-
-if (typeof TextDecoder !== 'undefined') { cachedTextDecoder.decode(); };
-
-let cachedUint8ArrayMemory0 = null;
-
-function getUint8ArrayMemory0() {
-    if (cachedUint8ArrayMemory0 === null || cachedUint8ArrayMemory0.byteLength === 0) {
-        cachedUint8ArrayMemory0 = new Uint8Array(wasm.memory.buffer);
-    }
-    return cachedUint8ArrayMemory0;
-}
-
-function getStringFromWasm0(ptr, len) {
-    ptr = ptr >>> 0;
-    return cachedTextDecoder.decode(getUint8ArrayMemory0().subarray(ptr, ptr + len));
-}
-
 const heap = new Array(128).fill(undefined);
 
 heap.push(undefined, null, true, false);
+
+function getObject(idx) { return heap[idx]; }
 
 let heap_next = heap.length;
 
@@ -33,7 +17,144 @@ function addHeapObject(obj) {
     return idx;
 }
 
-function getObject(idx) { return heap[idx]; }
+function debugString(val) {
+    // primitive types
+    const type = typeof val;
+    if (type == 'number' || type == 'boolean' || val == null) {
+        return  `${val}`;
+    }
+    if (type == 'string') {
+        return `"${val}"`;
+    }
+    if (type == 'symbol') {
+        const description = val.description;
+        if (description == null) {
+            return 'Symbol';
+        } else {
+            return `Symbol(${description})`;
+        }
+    }
+    if (type == 'function') {
+        const name = val.name;
+        if (typeof name == 'string' && name.length > 0) {
+            return `Function(${name})`;
+        } else {
+            return 'Function';
+        }
+    }
+    // objects
+    if (Array.isArray(val)) {
+        const length = val.length;
+        let debug = '[';
+        if (length > 0) {
+            debug += debugString(val[0]);
+        }
+        for(let i = 1; i < length; i++) {
+            debug += ', ' + debugString(val[i]);
+        }
+        debug += ']';
+        return debug;
+    }
+    // Test for built-in
+    const builtInMatches = /\[object ([^\]]+)\]/.exec(toString.call(val));
+    let className;
+    if (builtInMatches.length > 1) {
+        className = builtInMatches[1];
+    } else {
+        // Failed to match the standard '[object ClassName]'
+        return toString.call(val);
+    }
+    if (className == 'Object') {
+        // we're a user defined class or Object
+        // JSON.stringify avoids problems with cycles, and is generally much
+        // easier than looping through ownProperties of `val`.
+        try {
+            return 'Object(' + JSON.stringify(val) + ')';
+        } catch (_) {
+            return 'Object';
+        }
+    }
+    // errors
+    if (val instanceof Error) {
+        return `${val.name}: ${val.message}\n${val.stack}`;
+    }
+    // TODO we could test for more things here, like `Set`s and `Map`s.
+    return className;
+}
+
+let WASM_VECTOR_LEN = 0;
+
+let cachedUint8ArrayMemory0 = null;
+
+function getUint8ArrayMemory0() {
+    if (cachedUint8ArrayMemory0 === null || cachedUint8ArrayMemory0.byteLength === 0) {
+        cachedUint8ArrayMemory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachedUint8ArrayMemory0;
+}
+
+const cachedTextEncoder = (typeof TextEncoder !== 'undefined' ? new TextEncoder('utf-8') : { encode: () => { throw Error('TextEncoder not available') } } );
+
+const encodeString = (typeof cachedTextEncoder.encodeInto === 'function'
+    ? function (arg, view) {
+    return cachedTextEncoder.encodeInto(arg, view);
+}
+    : function (arg, view) {
+    const buf = cachedTextEncoder.encode(arg);
+    view.set(buf);
+    return {
+        read: arg.length,
+        written: buf.length
+    };
+});
+
+function passStringToWasm0(arg, malloc, realloc) {
+
+    if (realloc === undefined) {
+        const buf = cachedTextEncoder.encode(arg);
+        const ptr = malloc(buf.length, 1) >>> 0;
+        getUint8ArrayMemory0().subarray(ptr, ptr + buf.length).set(buf);
+        WASM_VECTOR_LEN = buf.length;
+        return ptr;
+    }
+
+    let len = arg.length;
+    let ptr = malloc(len, 1) >>> 0;
+
+    const mem = getUint8ArrayMemory0();
+
+    let offset = 0;
+
+    for (; offset < len; offset++) {
+        const code = arg.charCodeAt(offset);
+        if (code > 0x7F) break;
+        mem[ptr + offset] = code;
+    }
+
+    if (offset !== len) {
+        if (offset !== 0) {
+            arg = arg.slice(offset);
+        }
+        ptr = realloc(ptr, len, len = offset + arg.length * 3, 1) >>> 0;
+        const view = getUint8ArrayMemory0().subarray(ptr + offset, ptr + len);
+        const ret = encodeString(arg, view);
+
+        offset += ret.written;
+        ptr = realloc(ptr, len, offset, 1) >>> 0;
+    }
+
+    WASM_VECTOR_LEN = offset;
+    return ptr;
+}
+
+let cachedDataViewMemory0 = null;
+
+function getDataViewMemory0() {
+    if (cachedDataViewMemory0 === null || cachedDataViewMemory0.buffer.detached === true || (cachedDataViewMemory0.buffer.detached === undefined && cachedDataViewMemory0.buffer !== wasm.memory.buffer)) {
+        cachedDataViewMemory0 = new DataView(wasm.memory.buffer);
+    }
+    return cachedDataViewMemory0;
+}
 
 function dropObject(idx) {
     if (idx < 132) return;
@@ -45,6 +166,15 @@ function takeObject(idx) {
     const ret = getObject(idx);
     dropObject(idx);
     return ret;
+}
+
+const cachedTextDecoder = (typeof TextDecoder !== 'undefined' ? new TextDecoder('utf-8', { ignoreBOM: true, fatal: true }) : { decode: () => { throw Error('TextDecoder not available') } } );
+
+if (typeof TextDecoder !== 'undefined') { cachedTextDecoder.decode(); };
+
+function getStringFromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
+    return cachedTextDecoder.decode(getUint8ArrayMemory0().subarray(ptr, ptr + len));
 }
 /**
 */
@@ -98,10 +228,6 @@ async function __wbg_load(module, imports) {
 function __wbg_get_imports() {
     const imports = {};
     imports.wbg = {};
-    imports.wbg.__wbindgen_string_new = function(arg0, arg1) {
-        const ret = getStringFromWasm0(arg0, arg1);
-        return addHeapObject(ret);
-    };
     imports.wbg.__wbg_instanceof_Window_5012736c80a01584 = function(arg0) {
         let result;
         try {
@@ -116,13 +242,13 @@ function __wbg_get_imports() {
         const ret = getObject(arg0).document;
         return isLikeNone(ret) ? 0 : addHeapObject(ret);
     };
-    imports.wbg.__wbg_body_b3bb488e8e54bf4b = function(arg0) {
-        const ret = getObject(arg0).body;
-        return isLikeNone(ret) ? 0 : addHeapObject(ret);
-    };
     imports.wbg.__wbg_createElement_5921e9eb06b9ec89 = function() { return handleError(function (arg0, arg1, arg2) {
         const ret = getObject(arg0).createElement(getStringFromWasm0(arg1, arg2));
         return addHeapObject(ret);
+    }, arguments) };
+    imports.wbg.__wbg_querySelector_e21c39150aa72078 = function() { return handleError(function (arg0, arg1, arg2) {
+        const ret = getObject(arg0).querySelector(getStringFromWasm0(arg1, arg2));
+        return isLikeNone(ret) ? 0 : addHeapObject(ret);
     }, arguments) };
     imports.wbg.__wbg_setclassName_972f4bd6262f9c17 = function(arg0, arg1, arg2) {
         getObject(arg0).className = getStringFromWasm0(arg1, arg2);
@@ -169,6 +295,13 @@ function __wbg_get_imports() {
         const ret = getObject(arg0);
         return addHeapObject(ret);
     };
+    imports.wbg.__wbindgen_debug_string = function(arg0, arg1) {
+        const ret = debugString(getObject(arg1));
+        const ptr1 = passStringToWasm0(ret, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        const len1 = WASM_VECTOR_LEN;
+        getDataViewMemory0().setInt32(arg0 + 4 * 1, len1, true);
+        getDataViewMemory0().setInt32(arg0 + 4 * 0, ptr1, true);
+    };
     imports.wbg.__wbindgen_object_drop_ref = function(arg0) {
         takeObject(arg0);
     };
@@ -189,6 +322,7 @@ function __wbg_init_memory(imports, memory) {
 function __wbg_finalize_init(instance, module) {
     wasm = instance.exports;
     __wbg_init.__wbindgen_wasm_module = module;
+    cachedDataViewMemory0 = null;
     cachedUint8ArrayMemory0 = null;
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,81 @@
 use wasm_bindgen::prelude::*;
-use web_sys::{Document, Window};
-
+use web_sys::{Document, Element};
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
-    let document = web_sys::window()
-        .ok_or("Couldn't get the window object")?
-        .document()
-        .ok_or("Couldn't get the document object")?;
-    
-    let body = document.body().ok_or("Couldn't get the body element")?;
+    let document = web_sys::window().unwrap().document().unwrap();
 
-    // Create a header for the "About" section
-    let about_header = document.create_element("h1")?;
+    // Select the container div
+    let container = document.query_selector(".container").unwrap().unwrap();
+
+    // Create "About" header
+    let about_header: Element = document.create_element("h2")?;
     about_header.set_inner_html("About");
     about_header.set_class_name("about-header");
-    body.append_child(&about_header)?;
+    container.append_child(&about_header)?;
+
+    let linkedin_link = document.create_element("a")?;
+    linkedin_link.set_attribute("href", "https://www.linkedin.com/in/yourprofile")?;
+    linkedin_link.set_attribute("target", "_blank")?;
+    linkedin_link.set_inner_html("<i class='fab fa-linkedin'></i>");
+    container.append_child(&linkedin_link)?;
 
     // Create "About" paragraphs
     let about_paragraph1 = document.create_element("p")?;
-    about_paragraph1.set_inner_html("Lorem ipsum dolor sit amet...");
-    body.append_child(&about_paragraph1)?;
+    about_paragraph1.set_inner_html("Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+    anim id est laborum.");
+    container.append_child(&about_paragraph1)?;
 
     let about_paragraph2 = document.create_element("p")?;
-    about_paragraph2.set_inner_html("Consectetur adipiscing elit...");
-    body.append_child(&about_paragraph2)?;
+    about_paragraph2.set_inner_html("Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+    container.append_child(&about_paragraph2)?;
 
-    // Create main content section
+    // Create main content section with paragraphs and thumbnails
     for i in 1..=7 {
-        let paragraph = document.create_element("p")?;
+        // Create a container for each paragraph and thumbnail
+        let paragraph_container = document.create_element("div")?;
+        paragraph_container.set_class_name("paragraph-container");
+
+        // Create thumbnail image
         let thumbnail = document.create_element("img")?;
         thumbnail.set_attribute("src", &format!("assets/image{}.png", i))?;
+        thumbnail.set_attribute("alt", &format!("Thumbnail {}", i))?;
         thumbnail.set_class_name("thumbnail");
+        paragraph_container.append_child(&thumbnail)?;
 
-        let text = format!("This is the FIRST sentence in paragraph {}. More text follows...", i);
+        // Create paragraph
+        let paragraph = document.create_element("p")?;
+        let text = format!(
+            "{}. More text follows in this paragraph to make it sufficiently long.",
+            get_bold_first_sentence(i)
+        );
         paragraph.set_inner_html(&text);
-        body.append_child(&thumbnail)?;
-        body.append_child(&paragraph)?;
+        paragraph_container.append_child(&paragraph)?;
+
+        // Append the container to the main container
+        container.append_child(&paragraph_container)?;
     }
 
     Ok(())
+}
+
+// Helper function to generate the first sentence in all caps and bold
+fn get_bold_first_sentence(index: usize) -> String {
+    match index {
+        1 => "THIS IS THE FIRST SENTENCE IN PARAGRAPH ONE",
+        2 => "THIS IS THE FIRST SENTENCE IN PARAGRAPH TWO",
+        3 => "THIS IS THE FIRST SENTENCE IN PARAGRAPH THREE",
+        4 => "THIS IS THE FIRST SENTENCE IN PARAGRAPH FOUR",
+        5 => "THIS IS THE FIRST SENTENCE IN PARAGRAPH FIVE",
+        6 => "THIS IS THE FIRST SENTENCE IN PARAGRAPH SIX",
+        7 => "THIS IS THE FIRST SENTENCE IN PARAGRAPH SEVEN",
+        _ => "THIS IS A DEFAULT FIRST SENTENCE",
+    }
+    .to_string()
 }
 
 
