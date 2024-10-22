bbd5dba4631bc88f1e975b24f3e7595d3474fc5b
Eric <ericmachmer@factualfiction.com>
2024-10-21 21:59:09 -0700

    Refactor todo list formatting in main.rs

--- src/main.rs
+++ src/main.rs
@@ -22,26 +22,3 @@ fn main() {
     info!("starting app");
     launch(App);  // Launch the App from app.rs
 }
-
-/* **To Dos:**
-
-- [ ]  Cull screenshots of podcasts: xAI and PIML
-- [ ]  Oceanbluesky,io Rust WASM
-- [ ]  Read PIML
-- [ ]  Nvim: AstroVim
-- [ ]  Docker https://youtu.be/GFgJkfScVNU?si=1jvKC0gMuGy05EIJ
-- [ ]  GitHub Actions
-- [ ]  Apply xAI and QT Optimus
-- [ ]  Make shift rotator operational
-- [ ]  Register kindle devices
-- [ ]  Ask chatGPT for help setting up Notion databases with archived page
-    - [ ]  how to make linked databases for
-    - [ ]  Icons for subpages
-    - [ ]  Put monthly calendar in toggle
-    - [ ]  Bulletin boards with pinned boards
-    - [ ]  PARA  system
-    - [ ]  [https://boundscraps.notion.site/boundscraps/Notion-Global-Tag-Database-Step-by-Step-303626bf3f654fc6a53b3d0654353dc3](https://www.notion.so/303626bf3f654fc6a53b3d0654353dc3?pvs=21)
-    - [ ]  https://matthiasfrank.de/global-tags-in-notion/
-    - [ ]  Different views for same task database depending upon location for tasks
-    - [ ]  Save to Notion
-    - [ ]  Formulas for Notion */
