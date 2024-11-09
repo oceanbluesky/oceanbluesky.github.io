60c305ebda4e9e6bd3789c39533cf31aaaad3645
Eric <ericmachmer@factualfiction.com>
2024-11-08 18:30:36 -0800

    building out dioxus

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use dioxus::prelude::*;
+use dioxus_router::{Route, Router, Link};
+
 fn main() {
-    println!("Hello, world!");
+    dioxus_web::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx! {
+        Router {
+            nav {
+                Link { to: "/", "Home" }
+                Link { to: "/about", "About" }
+                Link { to: "/research", "Research" }
+                Link { to: "/micro-blog", "Micro-Blog" }
+                Link { to: "/contact", "Contact" }
+                Link { to: "/projects", "Projects" }
+            }
+            Route { to: "/", home::Home {} }
+            Route { to: "/about", about::About {} }
+            Route { to: "/research", research::Research {} }
+            Route { to: "/micro-blog", blog::Blog {} }
+            Route { to: "/contact", contact::Contact {} }
+            Route { to: "/projects", projects::Projects {} }
+        }
+    })
 }
