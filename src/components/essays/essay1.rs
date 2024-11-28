7ccaa6e74e7dbb4ddfaec037188020552fd5c935
Eric <ericmachmer@factualfiction.com>
2024-11-27 23:48:24 -0800

    modifying essay title fonts

--- src/components/essays/essay1.rs
+++ src/components/essays/essay1.rs
@@ -4,17 +4,19 @@ use dioxus::prelude::*;
 pub fn Essay1() -> Element {
     rsx! {
         div {
-            class: "ml-64 mt-16 mb-3",
-            h1 {
-                class: "font-display font-bold text-4xl text-black mb-4",
+            class: "about ml-64 mt-16 mb-3",
+            h2 {
+                class: "font-display font-bold text-5xl text-black",
                 "Essay 1: First Philosophical Exploration"
             }
             p {
-                class: "font-text text-lg text-gray-700 mb-4",
-                "This is the content of the first philosophical essay, exploring fundamental concepts and ideas."
+                class: "font-text text-lg text-gray-500",
+                "An exploration of fundamental philosophical concepts",
+                br {},
+                "delving into the core of intellectual inquiry."
             }
             div {
-                class: "font-text text-base text-gray-600",
+                class: "font-text text-base text-gray-600 mt-4",
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
             }
         }
